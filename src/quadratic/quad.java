c30d17b34c6ce87d0a157f9b18998595909f8148
Archi <archibk32@gmail.com>
2018-05-17 10:10:15 +0300

    Ver1

---
+++ src/quadratic/quad.java
@@ -0,0 +1,36 @@
+package quadratic;
+
+import java.io.*;
+import  java.util.*;
+
+public class quad {
+   void quad(int a, int b, int c){
+       //a*x^2+b*x^2+c =0 - квадратное уровнение
+// Вычисляем дискрименант
+       double res = (b*b) - (4*(a*c));
+       if (res>0){
+           System.out.println("D>0, значит уравнение имеет два корня");
+           double x1 = (-b + Math.sqrt(res))/2*a;
+           double x2 = (-b - Math.sqrt(res))/2*a;
+           System.out.println("Корень x1 = "+x1+" | "+"Корень x2 = "+x2);
+       } else if (res==0) {
+           System.out.println("D=0, значит уравнение имеет один корень");
+           double x3 = -b/2*a;
+           System.out.println("Корень x = "+x3);
+       } else if (res<0){
+           System.out.println("D<0, значит уравнение не имеет корней");
+       }
+       System.out.println(res);
+   }
+    public static void main(String[] args) {
+            quad discr = new quad();
+            Scanner in = new Scanner(System.in);
+        System.out.println("Enter A");
+        int w = in.nextInt();
+        System.out.println("Enter B");
+        int e = in.nextInt();
+        System.out.println("Enter C");
+        int r = in.nextInt();
+            discr.quad(w,e,r);
+    }
+}
